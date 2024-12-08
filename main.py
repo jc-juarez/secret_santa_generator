0d01f414899895bcdf9511361b36c7069fd81227
jc-juarez <goarp_jc@hotmail.com>
2024-12-07 21:15:15 -0800

    Add changes

--- main.py
+++
@@ -1,8 +0,0 @@
-from secret_santa_service import SecretSantaService
-
-def main():
-    secret_santa_service = SecretSantaService(SecretSantaService.SECRET_SANTA_FILE_NAME)
-    secret_santa_service.process_secret_santa_arrangement()
-
-if __name__ == '__main__':
-    main()
